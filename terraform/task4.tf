3832acd3c679641178f9b604d69f470912b54275
ekart-project <arfath29@git.com>
2025-06-19 17:57:02 +0530

    .

--- terraform/task4.tf
+++ terraform/task4.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "ap-south-1"
-}
+# provider "aws" {
+#   region = "ap-south-1"
+# }
 # resource "aws_instance" "task-5" {
 #   ami           = "ami-0f918f7e67a3323f0"
 #   instance_type = "t2.medium"
